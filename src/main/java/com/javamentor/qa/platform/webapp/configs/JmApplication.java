d57ab3df2e06cd8a0eafee9ad2f79bdeef269956
Tim <timur555@yandex.ru>
2022-08-01 17:06:08 +0300

    refactor

--- src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
@@ -1,31 +1,15 @@
 package com.javamentor.qa.platform.webapp.configs;
 
-import com.javamentor.qa.platform.webapp.configs.initializer.TestEntityInit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @ComponentScan("com.javamentor.qa.platform")
 @EntityScan("com.javamentor.qa.platform.models.entity")
 public class JmApplication {
-
-    private final Logger logger = LoggerFactory.getLogger(JmApplication.class);
-
-    @Autowired
-    private TestEntityInit testEntityInit;
-
     public static void main(String[] args) {
         SpringApplication.run(JmApplication.class, args);
-    }
-
-    private void testInit() {
-        testEntityInit.run();
     }
 }
